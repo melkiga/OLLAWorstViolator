cccaad29fd5ce8bd3f8873b233e124895718121d
melkiga <game194991@gmail.com>
2018-06-09 17:05:06 +0200

    > removed the templating for Matrix - now we only use sfmatrix (sparse matrix format) > deleted the dense matrix functions > moved the constructor, destructor, and non-inline functions to the .cc file from the header file

--- osvm/src/math/matrix.h
+++ osvm/src/math/matrix.h
@@ -23,127 +23,56 @@
 
 #include "numeric.h"
 #include "matrix_sparse.h"
-#include "matrix_dense.h"
 
-template<typename Matrix>
 class EvaluatorWorkspace {
-
-};
-
-
-template<>
-class EvaluatorWorkspace<dfmatrix> {
-public:
-	fvectorv urow;
-	fvectorv vrow;
-
-	fmatrixv matrixView;
-	fvectorv rowView;
-	fvectorv bufferView;
-	fvectorv x2View;
-
-	fvalue* matrixData;
-	size_t matrixTda;
-
-	sample_id* forwardMap;
-	sample_id* reverseMap;
-
-	EvaluatorWorkspace(dfmatrix* matrix);
-	~EvaluatorWorkspace();
-
-};
-
-
-template<>
-class EvaluatorWorkspace<sfmatrix> {
 public:
 	fvalue* buffer;
 
 	EvaluatorWorkspace(sfmatrix* matrix);
 	~EvaluatorWorkspace();
-
 };
 
 
 /*
  * Structure for holding the data (sparsely or densely) and other relevant information for kernel calculations.
  */
-template<typename Matrix>
 class MatrixEvaluator {
 
-	Matrix* matrix; // this would house the samples
+	sfmatrix* matrix; // this would house the samples
 	fvalue* x2; // ||x||^2 (this would be the squared 2-norm of a sample x)
-	EvaluatorWorkspace<Matrix> workspace;
+	EvaluatorWorkspace workspace;
 
 protected:
-	quantity getSize(Matrix* matrix);
-	quantity getDim(Matrix* matrix);
+	quantity getSize(sfmatrix* matrix);
+	quantity getDim(sfmatrix* matrix);
 
 	fvalue squaredNorm(sample_id v);
 
 public:
-	MatrixEvaluator(Matrix* matrix);
+	MatrixEvaluator(sfmatrix* matrix);
 	~MatrixEvaluator();
 
 	fvalue dot(sample_id u, sample_id v);
 
-	void dist(sample_id id, sample_id rangeFrom, sample_id rangeTo,
-			fvector* buffer);
-	void dist(sample_id id, sample_id rangeFrom, sample_id rangeTo,
-			sample_id* mappings, fvector* buffer);
+	void dist(sample_id id, sample_id rangeFrom, sample_id rangeTo, fvector* buffer);
+	void dist(sample_id id, sample_id rangeFrom, sample_id rangeTo, sample_id* mappings, fvector* buffer);
 	fvalue dist(sample_id u, sample_id v);
 
 	void swapSamples(sample_id u, sample_id v);
 
 };
 
-template<typename Matrix>
-MatrixEvaluator<Matrix>::MatrixEvaluator(Matrix* matrix) :
-		matrix(matrix),
-		workspace(matrix) {
-	quantity length = getSize(matrix);
-	x2 = new fvalue[length];
-	for (sample_id id = 0; id < length; id++) {
-		x2[id] = squaredNorm(id);
-	}
-}
-
-template<typename Matrix>
-MatrixEvaluator<Matrix>::~MatrixEvaluator() {
-	delete [] x2;
-}
-
-template<typename Matrix>
-void MatrixEvaluator<Matrix>::dist(sample_id id, sample_id rangeFrom, sample_id rangeTo,
-		sample_id* mappings, fvector* buffer) {
-	fvalue* data = buffer->data;
-	for (sample_id i = rangeFrom; i < rangeTo; i++) {
-		data[i] = dist(mappings[i], id);
-	}
-}
-
-/*
- * For samples u and v, this calculated the euclidean distance between the two.
- * returns: ||u - v||^2: ||u||^2 + ||v||^2 - 2*(<u,v>)
- */
-template<typename Matrix>
-fvalue MatrixEvaluator<Matrix>::dist(sample_id u, sample_id v) {
-	return x2[u] + x2[v] - 2 * dot(u, v);
-}
-
 /*
  * Returns the number of samples of the data (aka matrix height)
  */
-template<typename Matrix>
-inline quantity MatrixEvaluator<Matrix>::getSize(Matrix* matrix) {
+inline quantity MatrixEvaluator::getSize(sfmatrix* matrix) {
 	return (quantity) matrix->height;
 }
 
 /*
 * Returns the dimensionality of the data (number of attributes)
 */
-template<typename Matrix>
-inline quantity MatrixEvaluator<Matrix>::getDim(Matrix* matrix) {
+inline quantity MatrixEvaluator::getDim(sfmatrix* matrix) {
 	return (quantity) matrix->width;
 }
 
